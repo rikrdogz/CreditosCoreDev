66ff498b119dbcf5c91e93cbe21a0ad4f4962f31
Luis Ricardo Genovez Cruz <luisricardogz@gmail.com>
2022-04-05 13:14:26 -0500

    primer commit

---
+++ CreditosCore/Controllers/Clientes/ClientesModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreditosCore.Controllers.Clientes
+{
+    public class ClientesModel
+    {
+        public int idCliente { get; set; }
+        public string nombre { get; set; }
+        public string apellidoPaterno { get; set; }
+        public string apellidoMaterno { get; set; }
+        public string correoElectronico { get; set; }
+    }
+}
---
+++ CreditosCore/Database/SqlDataContext.cs
@@ -0,0 +1,15 @@
+﻿using CreditosCore.Controllers.Clientes;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreditosCore.Database
+{
+    public class SqlDataContext: DbContext
+    {
+        public DbSet<ClientesModel> clientes { get; set; }
+
+    }
+}
