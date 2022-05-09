4ae363bc017c0384b3cb6f0d013d5b24aac951b7
Luis Ricardo Genovez Cruz <luisricardogz@gmail.com>
2022-05-09 09:09:28 -0500

    ADD MVC

--- CreditosCore/Startup.cs
+++ CreditosCore/Startup.cs
@@ -88,6 +88,9 @@ namespace CreditosCore
                 });
 
             });
+
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
