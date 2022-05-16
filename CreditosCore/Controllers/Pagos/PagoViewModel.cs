5bb65c02949284aef56d39754e41b2b148017a02
Luis Ricardo Genovez Cruz <luisricardogz@gmail.com>
2022-05-16 16:00:03 -0500

    agregando pagos por cliente

---
+++ CreditosCore/Controllers/Pagos/PagoViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreditosCore.Controllers.Pagos
+{
+    public class PagoViewModel
+    {
+        public int idPago { get; set; }
+        public string nombre { get; set; }
+        public decimal monto { get; set; }
+        public decimal descuento { get; set; }
+        public decimal faltaPago { get; set; }
+        public string fechaPago { get; set; }
+        public string numeroPago { get; set; }
+    }
+}
